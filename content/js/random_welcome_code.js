5ca8b69527e389bdb6d3e2330c0e5b0da9bd1f42
Michal Bugno <michal.bugno@gmail.com>
2012-02-24 13:35:08 +0100

    Semantics added (if something should be hidden, we hide it by 'hidden' class) JS lint guys

--- content/js/random_welcome_code.js
+++ content/js/random_welcome_code.js
@@ -1,21 +1,21 @@
 $(function(){
-  var technologies = $('.welcome div')
-  var index = Math.floor((Math.random() * technologies.length - 1 ))
-  $(technologies.get(index)).show();
+  var technologies = $('.welcome div');
+  var index = Math.floor((Math.random() * technologies.length - 1 ));
+  $(technologies.get(index)).removeClass('hidden');
 
   $('span.content')
-     .filter(function(){
-        var self = $(this)
-        if(self.text().indexOf('Base CRM') != -1){
-          return self
+     .filter(function() {
+        var self = $(this);
+        if(self.text().indexOf('Base CRM') != -1) {
+          return self;
         }
      })
      .each(function(){
-       var self = $(this)
-       var html = self.html()
-       html = html.replace('Base CRM', '<a href="http://futuresimple.com/base">Base CRM</a>')
-       html = html.replace('QuoteBase', '<a href="http://futuresimple.com/quotes">QuoteBase</a>')
-       self.html(html)
+       var self = $(this);
+       var html = self.html();
+       html = html.replace('Base CRM', '<a href="http://futuresimple.com/base">Base CRM</a>');
+       html = html.replace('QuoteBase', '<a href="http://futuresimple.com/quotes">QuoteBase</a>');
+       self.html(html);
      });
 
 })
