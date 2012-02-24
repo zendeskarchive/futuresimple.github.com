671966950fc2c778f05fa486479466d9f7fce8d7
Marcin Bunsch <marcin@futuresimple.com>
2012-02-24 14:46:36 +0100

    Initial commit

---
+++ js/welcome_code.js
@@ -0,0 +1,21 @@
+$(function(){
+  var technologies = $('.welcome div');
+  var index = Math.floor((Math.random() * technologies.length - 1 ));
+  $(technologies.get(index)).show();
+
+  $('span.content')
+     .filter(function() {
+        var self = $(this);
+        if(self.text().indexOf('Base CRM') != -1) {
+          return self;
+        }
+     })
+     .each(function(){
+       var self = $(this)
+       var html = self.html()
+       html = html.replace('Base CRM', '<a href="http://futuresimple.com/base?ref=blog-lab">Base CRM</a>')
+       html = html.replace('QuoteBase', '<a href="http://futuresimple.com/quotes?ref=blog-lab">QuoteBase</a>')
+       self.html(html)
+     });
+
+})
